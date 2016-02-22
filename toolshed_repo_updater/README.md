46d2d699e80b82f6d95fe0e7e0fb915e74ba2366
slugger70 <slugger70@gmail.com>
2016-02-22 15:42:02 +1100

    Added a script to update the tool reviions in the tool list yaml file

---
+++ toolshed_repo_updater/README.md
@@ -0,0 +1,21 @@
+## toolshed_repo_updater.py
+
+This script will take in the yaml file of toolshed tools to be installed and checks their revisions. It prints --> stdout a tab delimited list of tools that have newer revisions in the appropriate toolshed. It also outputs a yaml file of the tools to be installed with all of the revisions updated.
+
+Example:
+
+      $ cd gvl.utilities/toolshed_repo_updater
+      $ python toolshed_repo_updater.py -i input.yaml -o output.yaml
+
+Usage:
+
+      usage: toolshed_repo_updater.py [-h] -i INPUT -o OUTPUT
+
+      optional arguments:
+      -h, --help            show this help message and exit
+
+      required arguments:
+      -i INPUT, --input INPUT
+                        Input file name
+      -o OUTPUT, --output OUTPUT
+                        Output file name
---
+++ toolshed_repo_updater/toolshed_repo_updater.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from bioblend import toolshed
+import time
+import yaml
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help="Input file name", required=True)
+parser.add_argument('-o','--output', help='Output file name', required=True)
+
+args = parser.parse_args()
+
+ts = toolshed.ToolShedInstance(url='https://toolshed.g2.bx.psu.edu')
+tts = toolshed.ToolShedInstance(url='https://testtoolshed.g2.bx.psu.edu')
+
+stream=open(args.input, 'r')
+tools = yaml.load_all(stream)
+tool_list=[]
+
+for tool in tools:
+    tool_list=tool['tools']
+tool_list
+
+print("Tool\tOwner\tCurrent\tLatest")
+counter = 0
+for item in tool_list:
+
+    tool_key = item['name']+'::'+item['owner']
+    old_version=item.get('revision', 'unknown')
+    new_version = "unknown"
+
+    if "test" in item['tool_shed_url']:
+        new_version = tts.repositories.get_ordered_installable_revisions(item['name'], item['owner'])[-1]
+    else:
+        new_version = ts.repositories.get_ordered_installable_revisions(item['name'], item['owner'])[-1]
+
+    if old_version != new_version:
+        print "%s\t%s\t%s\t%s" % (item['name'], item['owner'], old_version, new_version)
+        item['revision'] = new_version
+
+    counter += 1
+    if counter == 20:
+        time.sleep(5)
+        counter = 0
+
+with open(args.output, "w") as outfile:
+    yaml.dump(tool_list, outfile, default_flow_style=False)
